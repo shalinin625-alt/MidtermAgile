92c8610180523c657872eb15be14d9c8bfc4cc72
shalinin625-alt <shalini.n625@gmail.com>
2026-03-28 17:24:10 +0530

    improved factorial output

--- factroial.py
+++ factroial.py
@@ -15,4 +15,9 @@ def factorial(n):
 
 print("Factorial Program")   # NEW LINE
 num = 5                      # NEW LINE
-print("Factorial of", num, "is", factorial(num))   # MODIFIED
+print("Factorial of", num, "is", factorial(num))  
+
+
+
+hjhj
+ # MODIFIED
