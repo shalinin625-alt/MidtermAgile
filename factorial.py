303c445f4396d5227b3f5f6bd2f6c91957eea8e6
shalinin625-alt <shalini.n625@gmail.com>
2026-03-28 16:48:19 +0530

    io

---
+++ factorial.py
@@ -0,0 +1,13 @@
+# factorial.py
+
+num = int(input("Enter a number: "))
+
+fact = 1
+
+if num < 0:
+    print("Factorial not possible")
+else:
+    for i in range(1, num + 1):
+        fact = fact * i
+
+    print("Factorial of", num, "is", fact)
